7270aedc26afa039a1ad4205e8f0abaec10a2666
Seoyeon Kim <catneru@naver.com>
2018-11-13 19:01:24 +0900

    첫 프로젝트 생성

---
+++ app/src/main/java/catneru/kr/hs/mirim/nameilda/NameGalleryActivity.java
@@ -0,0 +1,13 @@
+package catneru.kr.hs.mirim.nameilda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NameGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_gallery);
+    }
+}
