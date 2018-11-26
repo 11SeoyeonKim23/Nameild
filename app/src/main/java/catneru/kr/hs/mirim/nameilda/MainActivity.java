2c57c1cd07cd593275815dca22f44c5e148277ca
Seoyeon Kim <catneru@naver.com>
2018-11-26 22:12:29 +0900

    기량아 syntax error 고쳐줘^^!!

--- app/src/main/java/catneru/kr/hs/mirim/nameilda/MainActivity.java
+++ app/src/main/java/catneru/kr/hs/mirim/nameilda/MainActivity.java
@@ -1,6 +1,7 @@
 package catneru.kr.hs.mirim.nameilda;
 
 import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
